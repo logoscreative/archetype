3f95a8c689b8853c7ffa45403bee9be4e533fc24
Cliff Seal <cliff@logos-creative.com>
2013-10-06 09:35:49 -0400

    Style search form with appended icon instead

--- searchform.php
+++ searchform.php
@@ -8,8 +8,10 @@
  */
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo apply_filters( 'archetype_search_action', home_url( '/' ) ); ?>">
-	<div class="form-group">
-		<input type="search" class="search-field form-control" placeholder="Search" value="" name="s" title="Search for:" />
-	</div>
-	<input type="submit" class="btn btn-default search-submit" value="Search" />
+    <div class="input-group">
+        <input type="search" class="search-field form-control" placeholder="Search" value="" name="s" title="Search for:" />
+        <span class="input-group-btn">
+            <button type="submit" class="btn btn-default search-submit"><i class="icon-search"></i></button>
+        </span>
+    </div>
 </form>
