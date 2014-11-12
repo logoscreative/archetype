a34e5f4c9a2adffd0d5d3424dda9338cc8856b64
Cliff Seal <cliff@logos-creative.com>
2014-11-12 08:37:08 -0500

    Integrete smarter shims and such

--- functions.php
+++ functions.php
@@ -49,6 +49,44 @@ function archetype_enqueue_bootstrap() {
 add_action( 'wp_enqueue_scripts', 'archetype_enqueue_bootstrap' );
 
 /* ----------------------------------------------------------- *
+ * Non-equeuable Scripts
+ * ----------------------------------------------------------- */
+
+function archetype_head_markup() {
+
+	// Touch Icon
+	$output = '<link rel="apple-touch-icon-precomposed" sizes="144x144" href="' . apply_filters( 'archetype_touch_icon', get_stylesheet_directory_uri() . '/assets/dist/img/touch-icon.png' ) . '" />';
+
+	if ( !defined('ARCHETYPE_ENQUEUE') ) {
+
+		// Help IE
+		$output .= '
+		<!--[if lt IE 9]>
+			<script type="text/javascript" src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script type="text/javascript" src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<link href="http://maxcdn.bootstrapcdn.com/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />
+			<link href="' . get_template_directory_uri() . '/cross-domain/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />
+			<script src="' . get_template_directory_uri() . '/cross-domain/respond.proxy.js"></script>
+			<![endif]-->';
+
+	} else {
+
+		// Help IE
+		$output .= '
+		<!--[if lt IE 9]>
+			<script type="text/javascript" src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script type="text/javascript" src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+			<![endif]-->';
+
+	}
+
+	echo $output;
+
+}
+
+add_action( 'wp_head', 'archetype_head_markup' );
+
+/* ----------------------------------------------------------- *
  * Theme Support & Registration
  * ----------------------------------------------------------- */
 
